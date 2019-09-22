2cc8694ea00159dc52e9804cfbff73f4c56f56d2
SoLetsCode <48116904+SoLetsCode@users.noreply.github.com>
2019-09-21 22:48:27 -0700

    javascript file
    javascript

---
+++ javascript/javascript.js
@@ -0,0 +1,22 @@
+function wordJumbler(sentence) {
+    let jumbledSentence = '';
+    let randomIndex = getRandomInt(sentence.length);
+    
+    while (sentence.length > 0) {
+        randomIndex = getRandomInt(sentence.length);
+        jumbledSentence += sentence.charAt(randomIndex);
+        sentence = removeLetter(sentence, randomIndex);
+    }
+
+    return jumbledSentence
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max))
+}
+
+function removeLetter(sentence, index) {
+    return sentence.substr(0,index) + sentence.substr(index+1)
+}
+
+document.getElementById('date').innerHTML = new Date().toDateString();
