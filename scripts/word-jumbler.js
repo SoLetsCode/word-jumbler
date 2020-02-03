83b84d7a9d8942104153321a4b0b5635cac87c09
Derek <derek@Dereks-MacBook-Pro.local>
2020-02-02 22:26:32 -0800

    finished site design and implemented dark mode

--- scripts/word-jumbler.js
+++ scripts/word-jumbler.js
@@ -1,9 +1,13 @@
 function wordJumbler(sentence) {
   let jumbledSentence = "";
-  let randomIndex = getRandomInt(sentence.length);
+  let randomIndex = getRandomInt(sentence.length - 1);
+
+  if (sentence.length === 0) {
+    return "Hey you didn't put anything in";
+  }
 
   while (sentence.length > 0) {
-    randomIndex = getRandomInt(sentence.length);
+    randomIndex = getRandomInt(sentence.length - 1);
     jumbledSentence += sentence.charAt(randomIndex);
     sentence = removeLetter(sentence, randomIndex);
   }
@@ -18,3 +22,36 @@ function getRandomInt(max) {
 function removeLetter(sentence, index) {
   return sentence.substr(0, index) + sentence.substr(index + 1);
 }
+
+// adding event listener for submit button
+
+document.querySelector(".form").addEventListener("submit", event => {
+  event.preventDefault();
+  let userInput = event.target.userinput.value;
+  event.target.reset();
+
+  document.querySelector(".output").innerHTML = wordJumbler(userInput);
+});
+
+//adding event listener for enter to send mesages.
+
+document.querySelector(".form__user-input").addEventListener("keyup", event => {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.querySelector(".form__input-button").click();
+  }
+});
+
+//adding event listener for dark mode button
+
+document.querySelector(".dark-mode-button").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("dark-mode-background");
+  document.querySelector(".title").classList.toggle("dark-mode-text");
+  document.querySelector(".output").classList.toggle("dark-mode-text");
+  document
+    .querySelector(".form__user-input")
+    .classList.toggle("dark-mode-user-input");
+});
